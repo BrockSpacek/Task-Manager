9fc770c11b3313d6fe71494d89584d266acece83
BrockSpacek <brockspacek1@gmail.com>
2025-02-14 10:45:44 -0800

    Local Storage and Popup

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,46 @@
+import { saveToLocalStorage, getFromLocalStorage, removeFromLocalStorage } from './localstorage.js';
 
+// IDs
+const addTaskBtn = document.getElementById("addTaskBtn");
+const toDoCard = document.getElementById("toDoCard");
+const inProgressCard = document.getElementById("inProgressCard");
+const completedCard = document.getElementById("completedCard");
+const taskPopup = document.getElementById("taskPopup")
+
+const saveTaskBtn = document.getElementById("saveTaskBtn");
+const cancelTaskBtn = document.getElementById("cancelTaskBtn");
+let taskNameInput = document.getElementById("taskNameInput");
+let taskDescriptionInput = document.getElementById("taskDescriptionInput");
+let taskPriorityInput = document.getElementById("taskPriorityInput");
+let taskDateInput = document.getElementById("taskDateInput");
 
 
 
 
 addTaskBtn.addEventListener('click', () => {
-    budgetPopup.classList.remove('hidden')
+    taskPopup.classList.remove('hidden')
 });
+
+cancelTaskBtn.addEventListener('click', () => {
+    taskPopup.classList.add('hidden');
+})
 
 
 saveTaskBtn.addEventListener('click', () => {
-    let Name = nameInput.value;
-    if (Name) {
-        saveToLocalStorage(Name);
-        nameInput.value = '';
-        displayNames();
+    let name = taskNameInput.value
+    let description = taskDescriptionInput.value
+    let priority = taskPriorityInput.value
+    let date = taskDateInput.value
+
+    if (name && description && priority && date) {
+        let task = { name, description, priority, date };
+        saveToLocalStorage(task);
+
+        // displayTasks();
+        taskPopup.classList.add('hidden');
+        taskNameInput.value = '';
+        taskDescriptionInput.value = '';
+        taskPriorityInput.value = '';
+        taskDateInput.value = '';
     }
 });
